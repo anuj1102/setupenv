26d92518fc29eac47d8719b526e9e0e920a324eb
Anuj Patel <anuj.patel@anuj-patel-mbp.local>
2018-08-05 13:31:26 -0700

    working

---
+++ setup.rb
@@ -0,0 +1,55 @@
+#!/usr/bin/ruby
+
+class Repo
+	attr_reader :dir, :url, :tags
+	def initialize(dir, url, tags)
+		@dir = dir
+		@url = url
+		@tags = tags
+	end
+	
+	def to_s
+		puts "Repo: [#{@dir}, #{@url}, #{@tags}]"
+	end
+
+	def add_subtree
+		run_system(subtree_cmd("add"))
+	end
+
+	private
+		def subtree_cmd(cmd)
+			"git subtree #{cmd} --prefix #{@dir} #{@url} master --squash"
+		end
+		
+		def run_system(cmd)
+			result = system(cmd)
+			puts "Failed cmd!: #{cmd}" unless result
+		end
+end
+
+oh_my_zsh = Repo.new(".oh-my-zsh", 
+										 "https://github.com/robbyrussell/oh-my-zsh",
+										 ["zsh"]
+										)
+
+zsh_custom = "#{oh_my_zsh.dir}/custom/plugins"
+
+command_time = Repo.new("#{zsh_custom}/command-time", 
+										 "https://github.com/popstas/zsh-command-time.git",
+										 ["zsh"]
+										)
+
+
+zsh_autosuggestions = Repo.new("#{zsh_custom}/zsh-autosuggestions", 
+										 "https://github.com/zsh-users/zsh-autosuggestions",
+										 ["zsh"]
+										)
+
+ZSH_REPOS = [oh_my_zsh, command_time, zsh_autosuggestions]
+
+command_time.add_subtree
+#puts ZSH_REPOS
+
+#def sutbtree_cmd(cmd)
+#	"git #{cmd} --prefix "
+#end
