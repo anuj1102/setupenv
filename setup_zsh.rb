20a4b06f9311694759c621f6cfbd05e1bcedfc58
Anuj Patel <anuj.patel@anuj-patel-mbp.local>
2018-08-05 18:54:45 -0700

    working

---
+++ setup_zsh.rb
@@ -0,0 +1,57 @@
+#!/usr/bin/ruby
+
+class Stage
+
+	def initialize(cmd, msg = "")
+		@cmd = cmd
+		@msg = msg
+	end
+
+	def ask
+		show(false)
+		puts " [press enter to continue]"
+		prompt = STDIN.gets
+		unless prompt == "y\n" || prompt == "\n"
+			puts "Quiting..."
+			exit 1
+		end
+		self
+	end
+
+	def wait(sleep_time)
+		sleep sleep_time
+		self
+	end
+
+	def run
+		puts @cmd
+		@result = system(@cmd)
+		puts "[Stage] Failed cmd!: #{@cmd}" unless @result
+		self
+	end
+
+	def fail
+		exit 1 unless @result
+		self
+	end
+
+	def show(newline = true)
+		s = @msg.empty? ? @cmd : @msg
+		if newline
+			puts s
+		else
+			print s
+		end
+		self
+	end
+end
+
+def stage(*args)
+	Stage.new(*args)
+end
+
+DOTFILES_DIR=File.expand_path(File.dirname(__FILE__))
+
+stage("cp ~/.zshrc ~/.zshrc.orig2", "Back up .zshrc?").ask.run
+stage("ln -s #{DOTFILES_DIR}/.zshrc ~/.zshrc", "Create symlink to .zshrc").ask.show.run
+
