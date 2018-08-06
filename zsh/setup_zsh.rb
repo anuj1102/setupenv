0a7200ec27dfbe2b234cc812e28e8ce143ea3202
Anuj Patel <anuj.patel@anuj-patel-mbp.local>
2018-08-06 08:32:13 -0700

    more updates2

--- zsh/setup_zsh.rb
+++ zsh/setup_zsh.rb
@@ -5,7 +5,7 @@ require_relative "setup_repos.rb"
 
 DOTFILES_DIR=File.expand_path(File.dirname(__FILE__))
 
-setup_repos(ZSH_REPOS)
+# setup_repos(ZSH_REPOS)
 stage("cp ~/.zshrc ~/.zshrc.orig", "Back up .zshrc?").ask.run
 stage("ln -s #{DOTFILES_DIR}/.zshrc ~/.zshrc", "Create symlink to .zshrc").ask.show.run
 stage("chsh -s /bin/zsh", "Change default shell to zsh").ask.show.run
