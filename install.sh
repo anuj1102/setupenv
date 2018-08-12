1784e83d2508fcc73e343a5bd2c983d79bc268fb
Anuj Patel <anuj.patel@anuj-patel-mbp.local>
2018-08-11 20:37:55 -0700

    Added comments

--- install.sh
+++ install.sh
@@ -3,14 +3,18 @@ function check_dep() {
 	command -v $1 >/dev/null 2>&1 || { echo >&2 "Dependency $1 not found"; exit 1; }
 }
 
+# OS must provide these through default packages
 check_dep ruby
 check_dep nvim # at least 0.3.0
 check_dep rg # ripgrep
 check_dep zsh
 check_dep pip3
 
+# Run ./packages/install_packages.rb to install these binaries locally
 check_dep ctags #--version should say universal
 check_dep abduco
 check_dep nvr
+
+# Update dotfiles and neovim plugins
 ./zsh/setup_zsh.rb
 ./neovim/setup_neovim.rb
