e379caa3f227c5c1f7295a78f2aa9500e076e924
Anuj Patel <anuj1102@gmail.com>
2019-02-19 16:03:42 +0000

    Fixed neovim install

--- install.sh
+++ install.sh
@@ -5,9 +5,14 @@ function check_dep() {
 
 # OS must provide these through default packages
 check_dep ruby
-check_dep rg # ripgrep
 check_dep zsh
 check_dep pip3
+check_dep wget
+check_dep curl
+
+
+# Optional but nice
+# check_dep rg # ripgrep
 
 # Run ./packages/install_packages.rb to install these binaries locally
 check_dep nvim "Run neovim/install.sh to set this up"
--- neovim/install.sh
+++ neovim/install.sh
@@ -1,5 +1,11 @@
+#!/bin/sh
 # Install neovim from https://github.com/neovim/neovim/releases
 
+LOCAL_PROG_PATH=$HOME/.local/bin
+cd $HOME
 wget https://github.com/neovim/neovim/releases/download/v0.3.4/nvim.appimage
+chmod u+x nvim.appimage
 ./nvim.appimage --appimage-extract
-ln -s $HOME/.local/bin/nvim squash-fs/AppRun # in home dir
+mkdir -p $LOCAL_PROG_PATH
+ln -s squashfs-root/AppRun $LOCAL_PROG_PATH/nvim
+cd -
