523e29c3f6eaec038e8c8d585a15457f0408afe9
Anuj Patel <anuj1102@gmail.com>
2019-02-19 16:08:33 +0000

    Fixed neovim install local symlink

--- neovim/install.sh
+++ neovim/install.sh
@@ -2,10 +2,9 @@
 # Install neovim from https://github.com/neovim/neovim/releases
 
 LOCAL_PROG_PATH=$HOME/.local/bin
-cd $HOME
 wget https://github.com/neovim/neovim/releases/download/v0.3.4/nvim.appimage
 chmod u+x nvim.appimage
 ./nvim.appimage --appimage-extract
 mkdir -p $LOCAL_PROG_PATH
-ln -s squashfs-root/AppRun $LOCAL_PROG_PATH/nvim
-cd -
+ln -sf $PWD/squashfs-root/AppRun $LOCAL_PROG_PATH/nvim
+rm nvim.appimage
