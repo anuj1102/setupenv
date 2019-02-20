8ebf7936e27da259abf2368687f46c99be848e1b
Anuj Patel <anuj@Anujs-MacBook-Pro.local>
2019-02-20 07:21:42 -0800

    added nvr git configs

--- neovim-remote/install.sh
+++ neovim-remote/install.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
 pip3 install --user neovim-remote
+git config --global core.editor 'nvr --remote-wait-silent'
+echo "Also add this to your ~/.gitconfig"
+echo """
+[diff]
+    tool = nvr
+[difftool "nvr"]
+    cmd = nvr -s -d $LOCAL $REMOTE
+[merge]
+    tool = nvr
+[mergetool "nvr"]
+    cmd = nvr -s -d $LOCAL $BASE $REMOTE $MERGED -c 'wincmd J | wincmd ='
+"""
