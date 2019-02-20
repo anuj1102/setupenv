375b14c51b56a115b96858df4e88a39b3a292767
Anuj Patel <anuj@Anujs-MacBook-Pro.local>
2019-02-19 21:45:28 -0800

    working

--- neovim/install.sh
+++ neovim/install.sh
@@ -2,6 +2,19 @@
 # Downlaod neovim from https://github.com/neovim/neovim/releases
 
 LOCAL_PROG_PATH=$HOME/.local/bin
+OSX_BINARY=neovim_binaries/nvim-osx64/bin/nvim
+
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+	BINARY=$LINUX_BINARY
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+	BINARY=$OSX_BINARY
+fi
+
+if [ -z "$BINARY" ]; then
+	  echo "Unable to determin host"
+    exit 1
+fi
+
 mkdir -p $LOCAL_PROG_PATH
-ln -sf $PWD/squashfs-root/AppRun $LOCAL_PROG_PATH/nvim
+ln -sf $PWD/ $LOCAL_PROG_PATH/nvim
 ./setup_neovim.rb
