68aa2254fdaec902bc7a9c8eb476327e0ed0ed02
Anuj Patel <anuj@Anujs-MacBook-Pro.local>
2019-02-19 21:37:06 -0800

    Added neovim app image for mac

--- neovim/install.sh
+++ neovim/install.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
-# Install neovim from https://github.com/neovim/neovim/releases
+# Downlaod neovim from https://github.com/neovim/neovim/releases
 
 LOCAL_PROG_PATH=$HOME/.local/bin
-wget https://github.com/neovim/neovim/releases/download/v0.3.4/nvim.appimage
-chmod u+x nvim.appimage
-./nvim.appimage --appimage-extract
 mkdir -p $LOCAL_PROG_PATH
 ln -sf $PWD/squashfs-root/AppRun $LOCAL_PROG_PATH/nvim
-rm nvim.appimage
 ./setup_neovim.rb
