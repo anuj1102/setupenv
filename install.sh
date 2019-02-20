69bdd0ef474fa4d2260d2ea3d2b8dcb754bcbe04
Anuj Patel <anuj@Anujs-MacBook-Pro.local>
2019-02-20 07:15:29 -0800

    Rework install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,10 @@ function check_dep() {
 	command -v $1 >/dev/null 2>&1 || { echo >&2 "Dependency $1 not found. $2"; exit 1; }
 }
 
+function manual_install() {
+	echo "Run cd $1 && ./install.sh to setup $1"
+}
+
 # OS must provide these through default packages
 check_dep ruby
 check_dep zsh
@@ -14,7 +18,7 @@ check_dep curl
 
 
 # Run ./packages/install_packages.rb to install these binaries locally
-check_dep nvim "Run cd neovim && ./install.sh to set this up"
-check_dep abduco "Run cd abduco && ./install.sh to set this up"
-check_dep nvr "Run cd neovim-remote && ./install.sh to set this up"
-./zsh/setup_zsh.rb
+manual_install zsh
+manual_install nvim
+manual_install abduco
+manual_install nvr
